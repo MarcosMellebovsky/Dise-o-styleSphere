dbf1723a055dc0f2a7404d210b37b01900c4bbd2
YANNICK LERNER <47801170@ort>
2024-06-06 16:31:16 -0300

    inscky

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
 document.getElementById('filter-icon').addEventListener('click', function() {
     this.classList.toggle('active');
 });
+
+
+    document.addEventListener("DOMContentLoaded", function() {
+        // Selecciona el elemento del dropdown
+        var dropdown = document.querySelector('.dropdown');
+
+        // Agrega un listener para el evento click en el dropdown
+        dropdown.addEventListener('click', function(event) {
+            // Detiene la propagación del evento para evitar que se cierre el dropdown
+            event.stopPropagation();
+        });
+
+        // Agrega un listener para el evento click en el documento
+        document.addEventListener('click', function() {
+            // Cierra el dropdown cuando se hace clic fuera de él
+            var dropdownMenu = dropdown.querySelector('.dropdown-menu');
+            dropdownMenu.classList.remove('show');
+        });
+    });
+
